d98095bcc8de1afb71270e3c3d22c7aa06f263dc
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-13 15:14:31 -0500

    commit tree be64cc1e4078c6c6c7768e3c61f44295227906af

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+     cats.push(name)
+}
+// console.log(destructivelyAppendCat("Bubbles"))
+// console.log(cats)
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+console.log(appendCat("bubbles"))
+console.log(cats)
+function prependCat(name){
+    return [name, ...cats]
+}
+
+console.log(prependCat('poppy'))
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
+
+console.log(removeLastCat(cats))
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+console.log(removeFirstCat())
